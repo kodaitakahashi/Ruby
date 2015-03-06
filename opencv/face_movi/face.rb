88570d4980039655694882468e9f5de56d99bf7e
kodaitakahashi <kodaitakahashi.kt@gmail.com>
2015-03-06 17:56:30 +0900

    master_up

--- opencv/face_movi/face.rb
+++ opencv/face_movi/face.rb
@@ -2,30 +2,45 @@
 require 'opencv'
 include OpenCV
 
-i=0
+img_count =0
 #分類器の指定
 detector = CvHaarClassifierCascade::load '/usr/local/Cellar/opencv/2.4.9/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'
 #カレントディレクトの移動
-Dir.chdir('./result')
+begin
+  Dir.chdir('./img')
+rescue => ex
+  puts "フォルダ移動失敗"
+  puts ex.message
+end
 #カレントディレクトで該当するすべてのファイルを対象にする
 Dir.glob('*.{jpg,jpeg,png}',File::FNM_CASEFOLD).each do |filename|
   puts"ファイルあり"
   begin
     image = IplImage.load(filename)
     puts("ファイル読み込み成功")
-  rescue
+    #imageをグレースケールに変換
+    grayimage=image.BGR2GRAY
+    #grayimageをヒストグラムの均一化にする
+    grayimage_equalize_hist = grayimage.equalize_hist
+    Dir.chdir("../result")
+    grayimage_equalize_hist.save(File::basename(filename))
+  rescue => ex
     puts("失敗")
-    return 1
+    puts ex.message
   end
+
   #検出された顔に対してどんな処理を行なうか
-  detector.detect_objects(image).each do |region|
+  detector.detect_objects(grayimage_equalize_hist).each do |region|
     begin
-    image.rectangle! region.top_left, region.bottom_right,:color => OpenCV::CvColor::Red
-      image.save "#{File::basename(filename)}"
+      #set_roiは引数に位置を指定しトリミングする
+      face = image.set_roi(region)
+        face.save "face_#{img_count}.png"
+      img_count+=1
     rescue => ex
       puts "#{Time.now} error"
       puts ex.message
     end
   end
   puts "終了"
+  Dir.chdir("../img")
 end
