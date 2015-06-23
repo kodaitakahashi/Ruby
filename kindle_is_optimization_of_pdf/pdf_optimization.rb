fc4f2bc3cbd7dfade92df8fa7a50a99d7a310b2d
kodaitakahashi <kodaitakahashi.kt@gmail.com>
2015-06-23 17:16:07 +0900

    画像のりサイズ 未完成

--- kindle_is_optimization_of_pdf/pdf_optimization.rb
+++ kindle_is_optimization_of_pdf/pdf_optimization.rb
@@ -1,48 +1,67 @@
 #coding:utf-8
 
 require 'rmagick'
+require 'prawn'
 include Magick
 
 class Pdf
-  attr_reader :loadpdf, :splitimages
 
   def initialize()
     @loadpdf = nil
     @splitimges = Array.new
+    @resizeimages = Array.new
   end
   def load(file = "")
     begin
       @loadpdf = Magick::ImageList.new(file)
-      print("PDFの読み込みが完了しました")
+      print "PDFの読み込みが完了しました\n"
     rescue
-      print "指定されたファイルがPDFじゃない可能性がある。あるいは指定されていません"
+      print "指定されたファイルがPDFじゃない可能性がある。あるいは指定されていません\n"
       return 1
     end
   end
   def split
     begin
-    @loadpdf.each do  |img|
+      @loadpdf.each_with_index do  |img|
         @splitimges.push(img)
       end
-      print "分割が完了しました"
+      print "分割が完了しました\n"
+      images_save
     rescue => ex
-      print "エラーが起きました => #{ex}"
+      print "エラーが起きました => #{ex}\n"
     end
   end
   def construct(out_fname="hogehoge")
     begin
-    Prawn::Document.generate(out_fname) do
-      @splitimgs.each do |img|
-        image img, :at => [-1*bounds.absolute_left,bounds.absolute_top],
-        :fit => [bounds.absolute_right+bounds.absolute_leftmbounds.absolute_top+bounds.absolute_bottom]
-        start_new_page
-      end
+      File.open(out_fname,"wb"){ |f|
+      @splitimges.each do |img|
+
+        end
+        }
+    rescue => ex
+      print "pdfの作成を失敗しました\n"
+      print $@
+      print ex
+    end
+  end
+  def images_save
+    if File::exists?("tmp") == false
+      Dir::mkdir("./tmp")
+      print "tmpフォルダを作成しました"
+    end
+    Dir.chdir("./tmp")
+    begin
+    @splitimges.each_with_index do |img,i|
+      img.write"#{i.to_s}.jpg"
       end
     rescue
-      print "pdfの作成を失敗しました"
+      print "画像の保存を失敗しました\n"
+      print "tmpフォルダを削除します"
+      Dir::rmdir("tmp")
     end
   end
 end
+        
 class Kindle < Pdf
   def initialize()
     @Resize_x = 658
@@ -51,13 +70,24 @@ class Kindle < Pdf
   end
   def kindle_img_resize
     begin
-    @splitimges.each do |img|
-      img.resize(@Resize_x,@Resize_y)
+      Dir.glob('*,{jpg}',File::FNM_CASEFOLD).each do |file|
+        @resizeimages.push(Magick::Image.read(file).resize(@Resie_x,@Resize_y))
       end
-      print "リサイズが完了しました"
     rescue => ex
-      print "リサイズ時にエラーが起きました => #{ex}"
+      print "リサイズ時にエラーが起きました => #{ex}\n"
+      return 0
     end
+    print "リサイズが完了しました \n"
+=begin
+    begin
+    @splitimges.each do |img|
+        img = img.resize(@Resize_x,@Resize_y)
+      end
+      print "リサイズが完了しました\n"
+    rescue => ex
+      print "リサイズ時にエラーが起きました => #{ex}\n"
+    end
+=end
   end
 end
 
